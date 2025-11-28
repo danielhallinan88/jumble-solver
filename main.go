c2f18203ceb699f8a2d6aca53e90e1dec9b4ee7d
Dan Hallinan <danielhallinan88@gmail.com>
2025-11-28 10:25:54 -0600

    http server

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
     "bufio"
     "fmt"
+    "log"
+    "net/http"
     "os"
+    "strings"
 )
 
 func main() {
@@ -11,19 +14,39 @@ func main() {
     if err != nil {
         panic(err)
     }
-    //fmt.Println("Dictionary loaded with", len(dict), "words")
 
-    testStr := "eth"
+    port := "8082"
+    if len(os.Args) > 1 {
+        port = os.Args[1]
+    }
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("%s %s from %s\n", r.Method, r.URL.Path, r.RemoteAddr)
+        path := strings.Trim(r.URL.Path, "/")
+        if path == "" {
+            fmt.Fprintln(w, "Hello! Use /<jumble> to solve a word.")
+            return
+        }
+
+        w.WriteHeader(http.StatusOK)
+        foundStrings := solveJumble(path, dict)
+        log.Printf("INFO: Found permutations for %s: %s", path, foundStrings)
+        fmt.Fprintln(w, "Found permutations for", path, ": ", foundStrings)
+    })
+
+    fmt.Printf("Server listening on http://localhost:%s\n", port)
+    http.ListenAndServe(":"+port, nil)
+}
+
+func solveJumble(jumble string, dict map[string]bool) ([]string) {
     var validPerms []string
-    for _, p := range permutations(testStr) {
+    for _, p := range permutations(jumble) {
         if dict[p] {
             validPerms = append(validPerms, p)
         }
-        //fmt.Println(p, dict[p])
     }
-    fmt.Println(validPerms)
+    return validPerms
 }
-
 
 func loadDictionary(path string) (map[string]bool, error) {
     f, err := os.Open(path)
